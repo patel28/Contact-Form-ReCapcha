a28fa3ad6dbc074ac95fcfb36db14f381f3b71b3
Ganesh Patel <patel.ganesh28@gmail.com>
2017-07-27 19:21:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<div class="container">
+
+
+    <div class="col-lg-offset-4 col-lg-4" id="panel">
+        <h2>Get In Touch</h2>
+
+        <form>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Name</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Email</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Mobile No</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Message</label>
+            </div>
+            <div class="group">
+                <center> <button type="submit" class="btn btn-warning">Send <span class="glyphicon glyphicon-send"></span></button></center>
+            </div>
+        </form>
+
+    </div>
+</div>
