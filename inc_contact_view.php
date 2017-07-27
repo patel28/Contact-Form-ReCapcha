e611fb6600bb5fef9b219c13cdf1007877f39807
Ganesh Patel <patel.ganesh28@gmail.com>
2017-07-27 20:25:37 +0530

    Html Form Added

---
+++ inc_contact_view.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="well well-sm">
+                <form class="form-horizontal" action="" method="post">
+                    <fieldset>
+                        <legend class="text-center">Contact us</legend>
+
+                        <!-- Name input-->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="name">Name</label>
+                            <div class="col-md-8">
+                                <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Email input-->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="email">E-mail</label>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+                            </div>
+                        </div>
+                        
+                        <!-- Contact Number-->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="email">Contact Number</label>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Message body -->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="message">Message</label>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+                            </div>
+                        </div>
+
+                        <!-- Form actions -->
+                        <div class="form-group">
+                            <div class="col-md-11 text-right">
+                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,43 +1,15 @@
-
-<div class="container">
-
-
-    <div class="col-lg-offset-4 col-lg-4" id="panel">
-        <h2>Get In Touch</h2>
-
-        <form>
-
-            <div class="group">
-                <input type="text" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Name</label>
-            </div>
-
-            <div class="group">
-                <input type="text" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Email</label>
-            </div>
-
-            <div class="group">
-                <input type="text" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Mobile No</label>
-            </div>
-
-            <div class="group">
-                <input type="text" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Message</label>
-            </div>
-            <div class="group">
-                <center> <button type="submit" class="btn btn-warning">Send <span class="glyphicon glyphicon-send"></span></button></center>
-            </div>
-        </form>
-
-    </div>
-</div>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Contact Form</title>'
+        
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="contact.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php
+        require_once './inc_contact_view.php';
+        ?>
+    </body>
+</html>
